b3ab548efdaf467410c26b3145961bd181b8c8f6
freef <mtfreeland@gmail.com>
2019-04-18 16:56:24 -0400

    change link color and text decoration

--- src/paint/Drawing.js
+++ src/paint/Drawing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Drawing = ({ id, user, title, img, likes, username, owner }) => {
   return (
     <Fragment>
-      <h2><Link to={{ pathname: `/drawings/${id}`, state: { user: user, id: id } }} id={id} >{title}</Link></h2>
+      <h2><Link className="drawlink" to={{ pathname: `/drawings/${id}`, state: { user: user, id: id } }} id={id} >{title}</Link></h2>
       <div className='drawingcontainer'>
         <img className='drawing' id={ id + owner } src={img} alt={title} />
       </div>
